cb37d8c3e9ccb087cce9c82a22cfb68c0ba1f773
carlotalcd <carlotalcd@gmail.com>
2025-12-25 15:01:54 +0100

    Ejercicio 6 : Creación de objetos pantalla

--- app/src/main/java/com/campusdigitalfp/filmoteca/MainActivity.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.tooling.preview.Preview
 import com.campusdigitalfp.filmoteca.ui.theme.FilmotecaTheme
+import androidx.compose.material3.Button
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.draw.clip
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,5 +53,37 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     FilmotecaTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun AboutScreen() {
+    Scaffold { paddingValues -> 
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Creada por : Carlota López-coronado Domingo")
+            Image(
+                painter = painterResource(id = R.drawable.images),
+                contentDescription = "Foto avatar",
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(CircleShape)
+                    .padding(16.dp)
+            )
+            Button(onClick = {}) {
+                Text(text = "Ir al sitio web")
+            }
+            Button (onClick = {}) {
+                Text(text = "Obtener soporte")
+            }
+            Button (onClick = {}) {
+                Text(text = "Volver")
+            }
+        }
     }
 }
